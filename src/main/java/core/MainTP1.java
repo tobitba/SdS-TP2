523670dbf732f6045f7a911b2dfbd4cef1cacae7
tobias <tpugliano@itba.edu.ar>
2025-08-16 20:05:52 -0300

    chore: remove radius from particle generator

--- src/main/java/core/MainTP1.java
+++ src/main/java/core/MainTP1.java
@@ -44,7 +44,7 @@ public class MainTP1 {
         if (generateParticles) {
             double particleRadius = Double.parseDouble(System.getProperty(PARTICLE_RADIUS));
             boolean fixedRadius = Boolean.parseBoolean(System.getProperty(FIXED_RADIUS));
-            ParticleGenerator.generate(n, l, particle -> grid.addParticle(particle, true), particleRadius, fixedRadius, 0.03);
+            ParticleGenerator.generate(n, l, particle -> grid.addParticle(particle, true), 0.03);
         } else {
             parseInput(grid, n);
         }
--- src/main/java/tools/ParticleGenerator.java
+++ src/main/java/tools/ParticleGenerator.java
@@ -7,21 +7,17 @@ import java.util.function.Consumer;
 
 public class ParticleGenerator {
 
-    public static void generate(int particleNumber, double gridSize, Consumer<Particle> consumer, double radius , boolean fixRadius, double speed) {
+    public static void generate(int particleNumber, double gridSize, Consumer<Particle> consumer, double speed) {
         Random random = new Random();
         random.setSeed(System.currentTimeMillis());
         double x;
         double y;
         double direction;
-        double r = radius;
         for (int i = 0; i < particleNumber; i++) {
             x = random.nextDouble() * gridSize;
             y = random.nextDouble() * gridSize;
             direction = random.nextDouble(-Math.PI, Math.PI);
-            if(!fixRadius){
-                r = random.nextDouble(0.1, radius);
-            }
-            consumer.accept(new Particle(x, y, r, direction));
+            consumer.accept(new Particle(x, y,0, direction));//TODO: remove radius from particle
         }
         Particle.setSpeed(speed);
     }
